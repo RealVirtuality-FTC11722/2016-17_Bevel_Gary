be5241ef297a2817207bfdb74eddb4c6a3bf99ad
Lyesome <32992449+Lyesome@users.noreply.github.com>
2017-11-05 17:57:53 -0700

    Update MechanumTest.java

--- MechanumTest.java
+++ MechanumTest.java
@@ -100,16 +100,16 @@ public class MechanumTest extends LinearOpMode {
             //rightMotor.setPower(-gamepad1.right_stick_y);
 
             double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-            double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+            double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) + Math.PI / 4;
             double rightX = gamepad1.right_stick_x;
-            final double v1 = r * Math.cos(robotAngle) + rightX;
-            final double v2 = r * Math.sin(robotAngle) - rightX;
-            final double v3 = r * Math.sin(robotAngle) + rightX;
-            final double v4 = r * Math.cos(robotAngle) - rightX;
+            final double v1 = r * Math.sin(robotAngle) + rightX;
+            final double v2 = r * Math.cos(robotAngle) - rightX;
+            final double v3 = r * Math.cos(robotAngle) + rightX;
+            final double v4 = r * Math.sin(robotAngle) - rightX;
 
-            motorFL.setPower(v3);
+            motorFL.setPower(v1);
             motorFR.setPower(v2);
-            motorBL.setPower(v1);
+            motorBL.setPower(v3);
             motorBR.setPower(v4);
 
 
